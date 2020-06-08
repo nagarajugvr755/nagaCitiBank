2e952a6e872fcb018b03e5bc4aa244207bc4f80d
NagarajuRGV <NagarajuRGV@NagarajuRGV-PC>
2020-06-08 09:22:04 -0500

    work by naga

--- 1JavaDemo/src/com/ganesh/Demo.java
+++ 1JavaDemo/src/com/ganesh/Demo.java
@@ -6,9 +6,15 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello LakshmiDevi");
 		System.out.println("ganesh baaaaaaaaavaaaaa");
+		System.out.println("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
 		
+		System.out.println("ammmaaaaaaaaaaaa");
+		
+		System.out.println("llllllllllllllllllllllllllllllllllllllll");
+		System.out.println("mammamamamamamamkakshajkshkhkjh");
 		
 		System.out.println("=============================");
+		System.out.println("=++++++++++++++++++++++++++++++++++++++12+++++++");
 	}
 	
 }
