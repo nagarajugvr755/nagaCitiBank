818527a6e79a6ec9b512c01c9bddc443a1fc97b4
NagarajuRGV <NagarajuRGV@NagarajuRGV-PC>
2020-06-08 10:25:30 -0500

    thamudu

--- 1JavaDemo/src/com/ganesh/Demo.java
+++ 1JavaDemo/src/com/ganesh/Demo.java
@@ -6,6 +6,10 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("amma--123");
 		System.out.println("nanna");
+		
+		
+		System.out.println(" thamudu ");
+		
 	}
 	
 }
