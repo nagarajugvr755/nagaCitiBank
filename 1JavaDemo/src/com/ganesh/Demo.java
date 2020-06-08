667fb5cc0333387e66ae0ac115d3b888ec54e925
NagarajuRGV <NagarajuRGV@NagarajuRGV-PC>
2020-06-08 08:40:40 -0500

    ok mama

--- 1JavaDemo/src/com/ganesh/Demo.java
+++ 1JavaDemo/src/com/ganesh/Demo.java
@@ -6,6 +6,9 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello LakshmiDevi");
 		System.out.println("ganesh baaaaaaaaavaaaaa");
+		
+		
+		System.out.println("ammmaaaaaaaaaaaa");
 	}
 	
 }
