171d65d02bc01d8b5fbde32e2e47c63a80772bd2
NagarajuRGV <NagarajuRGV@NagarajuRGV-PC>
2020-06-08 10:19:38 -0500

    amma

--- 1JavaDemo/src/com/ganesh/Demo.java
+++ 1JavaDemo/src/com/ganesh/Demo.java
@@ -4,17 +4,7 @@ public class Demo {
 
 	
 	public static void main(String[] args) {
-		System.out.println("hello LakshmiDevi");
-		System.out.println("ganesh baaaaaaaaavaaaaa");
-		System.out.println("mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
-		
-		System.out.println("ammmaaaaaaaaaaaa");
-		
-		System.out.println("llllllllllllllllllllllllllllllllllllllll");
-		System.out.println("mammamamamamamamkakshajkshkhkjh");
-		
-		System.out.println("=============================");
-		System.out.println("=++++++++++++++++++++++++++++++++22++++111++12+++++++");
+		System.out.println("amma");
 	}
 	
 }
